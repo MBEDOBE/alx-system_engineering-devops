c6ea7da75190f1c398905b752b94cca8d561dde0
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2023-04-24 18:22:38 +0000

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,18 +1,37 @@
 #!/usr/bin/python3
-"""script using this REST API, for a given employee ID,
-returns information about his/her TODO list progress."""
-import requests as r
+
+"""
+Retrieve and process the TODO list progress of a given employee ID using the provided REST API.
+"""
+
+import requests
 import sys
 
-if __name__ == '__main__':
-    url = 'https://jsonplaceholder.typicode.com/'
-    usr_id = r.get(url + 'users/{}'.format(sys.argv[1])).json()
-    to_do = r.get(url + 'todos', params={'userId': sys.argv[1]}).json()
-#    print(to_do)
-    completed = [title.get("title") for title in to_do if
-                 title.get('completed') is True]
-    print(completed)
-    print("Employee {} is done with tasks({}/{}):".format(usr_id.get("name"),
-                                                          len(completed),
-                                                          len(to_do)))
-    [print("\t {}".format(title)) for title in completed]
+
+def main():
+    """
+    Retrieve and process the TODO list progress of a given employee ID.
+    """
+    employee_id = sys.argv[1]
+
+    response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos")
+
+    if response.status_code != 200:
+        print("Error: Failed to retrieve TODO list.")
+        sys.exit(1)
+
+    todos = response.json()
+
+    total_tasks = len(todos)
+    done_tasks = sum(1 for todo in todos if todo.get("completed"))
+
+    print(f"Employee {todos[0].get('name')} is done with {done_tasks}/{total_tasks} tasks:")
+
+    for todo in todos:
+        if todo.get("completed"):
+            print(f"\t{todo.get('title')}")
+
+
+if __name__ == "__main__":
+    main()
+
