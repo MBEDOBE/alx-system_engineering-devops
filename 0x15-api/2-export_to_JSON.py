e6a3ff5821a1f7f6129cc0422e6bc89b8d09512b
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2023-04-24 17:04:13 +0000

    Update 2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,34 +1,19 @@
+#!/usr/bin/python3
+"""Exports to-do list information for a given employee ID to JSON format."""
+import json
 import requests
 import sys
-import json
 
-# Define the base URL of the API
-base_url = "https://jsonplaceholder.typicode.com"
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-# Get the employee ID from the command line arguments
-employee_id = int(sys.argv[1])
-
-# Send a GET request to the API to get the list of todos for the employee
-todos = requests.get(f"{base_url}/todos?userId={employee_id}").json()
-
-# Get the name of the employee from the API
-employee_name = requests.get(f"{base_url}/users/{employee_id}").json()['username']
-
-# Create a new dictionary to store the TODO list data
-todo_list = {"USER_ID": []}
-
-# Add a new item to the dictionary for each TODO item
-for todo in todos:
-    todo_item = {
-        "task": todo['title'],
-        "completed": todo['completed'],
-        "username": employee_name
-    }
-    todo_list["USER_ID"].append(todo_item)
-
-# Export the TODO list data to a JSON file with the employee ID as the filename
-filename = f"{employee_id}.json"
-with open(filename, mode='w') as json_file:
-    json.dump(todo_list, json_file, indent=4)
-
-print(f"TODO list for employee {employee_id} ({employee_name}) exported to {filename}")
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": username
+            } for t in todos]}, jsonfile)
