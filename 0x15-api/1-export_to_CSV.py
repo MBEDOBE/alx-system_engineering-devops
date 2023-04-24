52877b70d698a7814b860dd784cd33da9eef2922
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2023-04-24 17:02:51 +0000

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,29 +1,17 @@
 #!/usr/bin/python3
 """Script to export data in the CSV format"""
-import requests
-import sys
 import csv
+import requests as r
+import sys
 
-# Define the base URL of the API
-base_url = "https://jsonplaceholder.typicode.com"
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    usr = r.get(url + "users/{}".format(user_id)).json()
+    username = usr.get("username")
+    to_do = r.get(url + "todos", params={"userId": user_id}).json()
 
-# Get the employee ID from the command line arguments
-employee_id = int(sys.argv[1])
-
-# Send a GET request to the API to get the list of todos for the employee
-todos = requests.get(f"{base_url}/todos?userId={employee_id}").json()
-
-# Get the name of the employee from the API
-employee_name = requests.get(f"{base_url}/users/{employee_id}").json()['username']
-
-# Create a new CSV file with the employee ID as the filename
-filename = f"{employee_id}.csv"
-with open(filename, mode='w', newline='') as csv_file:
-    writer = csv.writer(csv_file)
-    writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-    
-    # Write a row for each TODO item
-    for todo in todos:
-        writer.writerow([employee_id, employee_name, todo['completed'], todo['title']])
-
-print(f"TODO list for employee {employee_id} ({employee_name}) exported to {filename}")
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow([user_id, username, elm.get("completed"),
+                          elm.get("title")]) for elm in to_do]
