b531fabc92b333f6abde826015e46582e641cd96
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2023-05-09 08:31:22 +0000

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,15 @@
+#!/usr/bin/python3
 import requests
+"""
+a function that queries reddit api and returns number
+of subs
+"""
 
 
 def number_of_subscribers(subreddit):
+    """
+    function definition of getting number of subs
+    """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
