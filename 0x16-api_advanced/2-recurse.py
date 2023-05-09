8292d3f3413fcff80449d27bced44f54c957a249
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2023-05-09 08:33:43 +0000

    Update 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,7 +1,16 @@
+#!/usr/bin/python3
 import requests
-
+"""
+function that queries reddit api
+"""
 
 def recurse(subreddit, hot_list=[], after=None):
+    """
+    recursive function that queries the reddit api and returns a list
+    containing the titles of all hot articles for a given reddit.
+    if no results are found for the given subreddit the function
+    should return none
+    """
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {"User-Agent": "Mozilla/5.0"}
     params = {"limit": 100}
