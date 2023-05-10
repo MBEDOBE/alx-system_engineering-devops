fb2f8ebf30b389603243eafa86ef5f2ea8e0535d
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2023-05-10 02:47:22 +0000

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
 import requests
 """
-a functions that queries the reddit api
+functions that queries the reddit api
 """
+
+
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "Mozilla/5.0"
-    }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
-        print("None")
+    """
+    function definition that prints the titles of first 10 hot posts listed
+    for a given subreddit
+    """
+    url = "https://api.reddit.com/r/{}?sort=hot&limit=10".format(subreddit)
+    header = {'User-Agent': 'CustomClient/1.0'}
+    req = requests.get(url, headers=header, allow_redirects=False)
+    if req.status_code != 200:
+        print(None)
         return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    req = req.json()
+    if "data" in req:
+        for post in req.get("data").get("children"):
+            print(post.get("data").get("title"))
+    else:
+        print(None)
